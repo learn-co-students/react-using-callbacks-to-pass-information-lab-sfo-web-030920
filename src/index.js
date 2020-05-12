78712426ca0c267b89a3410d4827818ff88b693a
caicindy87 <caicindy87@gmail.com>
2020-05-12 16:21:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import learnSymbol from './data.js'
-import Matrix from './Matrix.js'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<Matrix values={learnSymbol} />, document.getElementById('root'));
+import "./index.css";
+import learnSymbol from "./data.js";
+import Matrix from "./Matrix.js";
+
+ReactDOM.render(
+  <Matrix values={learnSymbol} />,
+  document.getElementById("root")
+);
